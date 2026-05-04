b56420f1e4f2c4d1a9147b4ea10bb7a570951ae1
Affan <affanraze@gmail.com>
2026-05-05 02:10:49 +0500

    css fix

--- src/data/art.js
+++ src/data/art.js
@@ -1,27 +1,27 @@
 export const artworks = [
   {
     id: 1,
-    title: 'Anime Landscape 1',
+    title: '',
     src: '/assets/MyArt/123123.jpeg',
   },
   {
     id: 2,
-    title: 'Cyberpunk City',
+    title: '',
     src: '/assets/MyArt/Art1.png',
   },
   {
     id: 3,
-    title: 'Lofi Room',
+    title: '',
     src: '/assets/MyArt/Art2.png',
   },
   {
     id: 4,
-    title: 'Neon Nights',
+    title: '',
     src: '/assets/MyArt/asdla.jpeg',
   },
   {
     id: 5,
-    title: 'Journey Deviant',
+    title: '',
     src: '/assets/MyArt/Journey Deviant.jpeg',
   }
 ];
